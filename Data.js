04021235fb554b1b3df9c73ed7fcd80ab23786ac
Someleze1412 <133154141+Someleze1412@users.noreply.github.com>
2023-09-21 14:59:50 +0200

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 100,
+      desc: "Basic shirt.",
+      img: "https://www.side-step.co.za/media/catalog/product/cache/f8df61b05911d279f59846ba6dcb8724/c/h/cha45n-champion-crewneck-tshirt-navy-218550-v1_jpg.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 150,
+      desc: "Unisex navy Golfer.",
+      img: "https://thefoschini.vtexassets.com/arquivos/ids/73675912-800-800?v=638280358185030000&width=800&height=800&aspect=true",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 170,
+      desc: "Mens barron pique knit Golfer.",
+      img: "https://cdn1.brandability.co.za/2020/11/20110755/Promo-T-Shirt-Black-510x510.jpg",
+    },
+    {
+        id: "thyfhcbcv",
+        name: "Mens Suit",
+        price: 200,
+        desc: "Mens basic pique shirt",
+        img: "https://cdn1.brandability.co.za/2015/12/12094355/Mens-Basic-Pique-Golf-Shirt-Light-Blue.jpg",
+      },
+  ]; 
